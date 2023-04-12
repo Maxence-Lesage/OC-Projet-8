0be4fada7a0ce5882a7cfc7b18ca30e837e17269
Maxence Lesage <maxencelesage4@gmail.com>
2023-04-12 12:34:35 +0200

    Navbar responsive + accommodation list alignment bug fix

--- src/pages/error404/Error404.js
+++ src/pages/error404/Error404.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import '../Base.css';
 import './Error404.css';
 import Navbar from '../../components/navbar/Navbar';
@@ -7,6 +8,11 @@ function Error404() {
     return (
         <>
             <Navbar />
+            <div className="errorMessage">
+                <p className="errorMessage_code">404</p>
+                <h1 className="errorMessage_text">Oups! La page que vous demandez n'existe pas.</h1>
+                <h2 className="backToHome"><Link className="link" to={"/"}>Retourner sur la page d'accueil</Link></h2>
+            </div>
         </>
     );
 }
